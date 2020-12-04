240d29c40e1c655c44dd2f1e427882c2513f4058
Merijn Verstraaten <merijn@inconsistent.nl>
2020-12-04 18:15:16 +0100

    Day 4, puzzle 2

--- Day4.hs
+++ Day4.hs
@@ -5,17 +5,21 @@ module Main(main) where
 
 import Control.Monad (void)
 import Data.Bifunctor (second)
-import Data.Char (isSpace)
+import Data.Char (isSpace, isDigit, isHexDigit)
 import Data.Foldable (asum)
 import Data.Map (Map)
 import qualified Data.Map as M
 import Data.Monoid (All(..))
+import Data.Set (Set)
+import qualified Data.Set as S
 import Data.Text (Text)
+import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import Data.Void (Void)
 import System.Environment (getArgs)
 import System.Exit (exitFailure)
 import System.IO (hPutStrLn, stderr)
+import Text.Read (readMaybe)
 
 import Text.Megaparsec (Parsec, runParser)
 import qualified Text.Megaparsec as Mega
@@ -82,8 +86,8 @@ blankLines = Mega.skipSome Mega.eol
 multiDocParser :: Parser [Map DocumentField Text]
 multiDocParser = Mega.sepEndBy1 docParser blankLines <* Mega.eof
 
-isValidPassport :: Map DocumentField v -> Bool
-isValidPassport = getAll . mconcat
+isValidPassport1 :: Map DocumentField v -> Bool
+isValidPassport1 = getAll . mconcat
     [ All . M.member BirthYear
     , All . M.member IssueYear
     , All . M.member ExpirationYear
@@ -93,6 +97,45 @@ isValidPassport = getAll . mconcat
     , All . M.member PassportID
     ]
 
+isValidPassport2 :: Map DocumentField Text -> Bool
+isValidPassport2 doc = getAll $ foldMap checkField
+    [ BirthYear
+    , IssueYear
+    , ExpirationYear
+    , Height
+    , HairColor
+    , EyeColor
+    , PassportID
+    ]
+  where
+    checkField :: DocumentField -> All
+    checkField f = case M.lookup f doc of
+        Nothing -> All False
+        Just txt -> validateField f $ T.unpack txt
+
+validateField :: DocumentField -> String -> All
+validateField field txt = All $ case field of
+    BirthYear -> checkRange 1920 2020 txt
+    IssueYear -> checkRange 2010 2020 txt
+    ExpirationYear -> checkRange 2020 2030 txt
+    EyeColor ->  S.member txt eyeColours
+    PassportID -> length txt == 9 && all isDigit txt
+    CountryID -> True
+    HairColor -> case txt of
+        '#':cs -> all isHexDigit cs
+        _ -> False
+
+    Height -> case span isDigit txt of
+        (n, "cm") -> checkRange 150 193 n
+        (n, "in") -> checkRange 59 76 n
+        _ -> False
+  where
+    checkRange :: Int -> Int -> String -> Bool
+    checkRange lo hi s = maybe False (\i -> i >= lo && i <= hi) $ readMaybe s
+
+    eyeColours :: Set String
+    eyeColours = S.fromList ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
+
 main :: IO ()
 main = do
     args <- getArgs
@@ -100,4 +143,5 @@ main = do
         [inputFile] -> parseFile inputFile multiDocParser
         _ -> hPutStrLn stderr "No input file!" >> exitFailure
 
-    print . length $ filter isValidPassport inputData
+    print . length $ filter isValidPassport1 inputData
+    print . length $ filter isValidPassport2 inputData
